f04efb8119074ccfea1881c5d288be268d33c4a5
loudclouds <loudclouds@geologist.com>
2015-09-28 13:42:49 -0500

    Adding the library

---
+++ README.md
@@ -0,0 +1,3 @@
+# numbers-that-r-cool
+
+For all of your cool number needs
---
+++ src/index.js
@@ -0,0 +1,16 @@
+var uniqueRandomArray = require('unique-random-array');
+
+var numbers = [
+  934534534,
+  7234,
+  004350450,
+  42348399444,
+  7,
+  52343240,
+  26347586
+];
+
+module.exports = {
+  all: numbers,
+  random: uniqueRandomArray(numbers)
+};
